6b68a831d69d58a798d1a7466cb6b2e275dbf7d0
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-08-25 14:49:40 -0700

    Project setup

---
+++ DynamicTickRate/DynamicTickRate.cs
@@ -0,0 +1,19 @@
+﻿using FrooxEngine;
+using ResoniteModLoader;
+
+namespace DynamicTickRate;
+
+public partial class DynamicTickRate : ResoniteMod
+{
+    public override string Name => "DynamicTickRate";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/DynamicTickRate";
+    public static ModConfiguration? Config;
+
+    public override void OnEngineInit()
+    {
+        Config = GetConfiguration();
+        Config?.Save(true);
+    }
+}
